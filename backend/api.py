db747b3e931042d21a0c92dec23944677916364a
HartleIG29 <HartleIG29@uww.edu>
2023-11-08 11:53:06 -0600

    initial api backend

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,15 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import uvicorn
 
+app = FastAPI()
+
+async def stream():
+    for i in range(10):
+        yield b"byte_stream"
+
+
+@app.get("/")
+async def main():
+    return StreamingResponse(stream())
