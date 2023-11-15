d1b6dd620b5b7673ceab2e18a0cb41fdc51fe1c8
b-splittgerber <brock.splittgerber@gmail.com>
2023-11-15 10:55:26 -0600

    Update App.js and App.test.js files

--- text-adventure-react/src/App.js
+++ text-adventure-react/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
+import StartupPage from './StartupPage';
+import AdventurePage from './AdventurePage';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [showAdventurePage, setShowAdventurePage] = useState(false);
+  const [gameOptions, setGameOptions] = useState(null);
+
+  const handleStartGame = (options) => {
+    setGameOptions(options);
+    setShowAdventurePage(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showAdventurePage ? (
+        <AdventurePage gameOptions={gameOptions} />
+      ) : (
+        <StartupPage onStartGame={handleStartGame} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- text-adventure-react/src/App.test.js
+++ text-adventure-react/src/App.test.js
@@ -1,27 +1,8 @@
-// App.js
-import React, { useState } from 'react';
-import StartupPage from './StartupPage';
-import AdventurePage from './AdventurePage';
-import './App.css';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-const App = () => {
-  const [showAdventurePage, setShowAdventurePage] = useState(false);
-  const [gameOptions, setGameOptions] = useState(null);
-
-  const handleStartGame = (options) => {
-    setGameOptions(options);
-    setShowAdventurePage(true);
-  };
-
-  return (
-    <div>
-      {showAdventurePage ? (
-        <AdventurePage gameOptions={gameOptions} />
-      ) : (
-        <StartupPage onStartGame={handleStartGame} />
-      )}
-    </div>
-  );
-};
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  //const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
