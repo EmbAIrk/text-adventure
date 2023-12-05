19b4dc7fc5fa05832b6db6dffa0f31769a32747d
nashnj13 <20nashna@gmail.com>
2023-12-04 19:37:47 -0600

    jerm update

--- backend/Database.py
+++ backend/Database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
+import random
+import string
 
 class Database:
     def __init__(self, db_url):
@@ -7,16 +9,46 @@ class Database:
         self.session = Session(self.engine)
 
     def execute_query(self, query, params=None):
+        '''
+        Executes given query on database
+        
+        Args:
+            query (string): actual query
+            params (dict): dictionary of parameters
+            
+        Returns:
+            result of executed query
+        '''
         with self.session.begin() as connection:
             result = connection.execute(text(query), params)
         return result
 
-    def get_save_data(self, key):
-        query = "EXEC GetSaveData :key"
-        result = self.execute_query(query, params={"key": key})
+    def fetch_data(self, id):
+        '''
+        Fetches given data from database
+        
+        Args:
+            id (string): id of data in db
+            
+        Returns:
+            context and notes from database
+        '''
+        query = "EXEC FetchData :id"
+        result = self.execute_query(query, params={"id": id})
         return result.fetchone()
 
-    def save_data(self, key, data):
-        query = "EXEC SaveData :key, :data"
-        self.execute_query(query, params={"key": key, "data": data})
-        return key
+    def save_data(self, context, notes):
+        '''
+        Stores given data in database
+        
+        Args:
+            context (list): list of dictionaries that represent the context
+            notes (string): information from notes area 
+            
+        Returns:
+            string : id of where data was stored
+        '''
+        id = ''.join(random.choices(string.ascii_letters, k=20))
+        query = "EXEC SaveData :id, :context, :notes"
+        self.execute_query(query, params={"id": id, "context": context, "notes": notes})
+        return id
